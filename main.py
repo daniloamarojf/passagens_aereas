568d5373449871182e491df32de32bb2ff2b9e4b
daniloamarojf <daniloramarojf@gmail.com>
2024-12-06 21:34:23 -0300

    cabecalho com erro

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from passagens.tabelas import criar_clientes
+from passagens.funcoes import cabecalho
 import sqlite3
 import os
 
 
 criar_clientes()
 
+cabecalho('CLIENTE')
 
--- passagens/funcoes.py
+++ passagens/funcoes.py
@@ -2,3 +2,16 @@ import sqlite3
 import os
 
 
+def cabecalho(nome_cabecalho):
+    os.system('cls')
+    print(f'::::::::::  {nome_cabecalho}  ::::::::::\n\n')
+
+def crud():
+    while True:
+    
+        print('1 - Adicionar Cliente\n')
+        print('2 - Alterar dados do cliente\n')  
+        print('3 - Remover Cliente\n')  
+        print('4 - Visualizar Clientes\n')
+
+
